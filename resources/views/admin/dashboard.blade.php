e3293f6548c289c4faa1f2dba413e952fdb3bfb4
ali9585 <114659707+ali9585@users.noreply.github.com>
2022-11-14 17:13:57 +0500

    admin panel initial2

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('admin.layouts.main')
+
+@push('title')
+    Dash
+@endpush
+
+@section('main-content')
+    
+    main
+
+@endsection
---
+++ resources/views/admin/layouts/main.blade.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- Title --}}
+    <title>
+
+        @stack('title')
+
+    </title>
+
+    {{-- Bootstrap --}} {{-- have to remove --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    {{-- Admin Panel --}}
+    <link href="{{ asset('assets/admin/img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('assets/admin/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/admin/css/style.css') }}" rel="stylesheet">
+
+
+    {{-- Styles --}}
+
+
+  </head>
+  <body>
+    
+    <div class="row">
+
+        <div class="col-3 bg-white">
+
+            @extends('admin.layouts.sidebar')
+
+        </div>
+
+        <div class="col-9">
+
+            @yield('main-content')
+
+        </div>
+
+    </div>
+
+    {{-- Admin Panel --}}
+    <script src="{{ asset('assets/admin/vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/chart.js/chart.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('assets/admin/js/main.js') }}"></script>
+    
+    {{-- Bootstrap --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+    {{-- Custom --}}
+    @stack('scripts')
+
+  </body>
+</html>
