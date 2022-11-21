dfa998bdd521ee86252602a310ac90feda22381c
Khizar wahab <khizarwahab19@gmail.com>
2022-11-21 10:53:48 +0500

    Add services scafolding

---
+++ database/migrations/2022_11_21_053558_create_service_packages_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedInteger('quantity')->nullable();
+            $table->integer('level')->default(1);
+            $table->string('level_name')->nullable();
+            $table->double('price');
+            $table->double('sale_price')->nullable();
+            $table->json('features')->nullable();
+            $table->unsignedBigInteger('service_id');
+            $table->unsignedBigInteger('service_category_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_packages');
+    }
+}
