dc8a1642ef1afed7896e66b7068811a52f0859a3
wadood <wanikhan919@gmail.com>
2022-11-24 11:08:04 +0500

    add daily speed to subscriptions

--- database/migrations/2022_11_24_060409_add_daily_speed_to_subscriptions_table.php
+++ database/migrations/2022_11_24_060409_add_daily_speed_to_subscriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDailySpeedToOrdersTable extends Migration
+class AddDailySpeedToSubscriptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddDailySpeedToOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::table('subscriptions', function (Blueprint $table) {
             $table->string('daily_speed')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddDailySpeedToOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::table('subscriptions', function (Blueprint $table) {
             $table->dropColumn('daily_speed');
         });
     }
